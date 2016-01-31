ff66b795699d4fe735611c4a5d033887c759e7bb
Lorenzo Hernandez <enzosarin@gmail.com>
2016-01-31 19:25:52 +0000

    nitrous-backup at Sun Jan 31 19:25:52 UTC 2016

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,68 @@
-# Write your code here!
+def game_hash
+    {:home =>
+    {:team_name=>"Brooklyn Nets",
+     :colors=>["Black", "White"],
+     :players=>
+     {"Alan Anderson"=>
+     {:number=>0,
+     :shoe=>16,
+     :num_points_scored=>22,
+     :rebounds=>12,
+     :assists=>12,
+     :steals=>3,
+     :blocks=>1,
+     :slam_dunks=>1
+     },
+        "Reggie Evans"=>
+        {:number=>30,
+     :shoe=>14,
+     :num_points_scored=>12,
+     :rebounds=>12,
+     :assists=>12,
+     :steals=>12,
+     :blocks=>12,
+     :slam_dunks=>7
+     },
+     "Brook Lopez"=>
+     {:number=>11,
+     :shoe=>17,
+     :num_points_scored=>17,
+     :rebounds=>19,
+     :assists=>10,
+     :steals=>3,
+     :blocks=>1,
+     :slam_dunks=>15
+     },
+     "Mason Plumlee"=>
+     {:number=>11,
+     :shoe=>17,
+     :num_points_scored=>17,
+     :rebounds=>19,
+     :assists=>10,
+     :steals=>3,
+     :blocks=>1,
+     :slam_dunks=>15
+     },
+     "Jason Terry"=>
+     {:number=>31,
+     :shoe=>15,
+     :num_points_scored=>19,
+     :rebounds=>2,
+     :assists=>2,
+     :steals=>4,
+     :blocks=>11,
+     :slam_dunks=>1}
+     }
+     },
+     :away=>
+     {:team_name=>"Charlotte Hornets",
+      :colors=>["Turquoise", "Purple"],
+      :players=>""
+     }
+    }
 
+
+end
 
 
 
